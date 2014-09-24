3eb7eb4bdcd94f3be8b09e7fb619788e8ee85430
Graham Ollis <perl@wdlabs.com>
2014-09-24 09:25:56 -0400

    initial implementation

---
+++ README.md
@@ -0,0 +1,21 @@
+# Alien::nasm
+
+Find or build nasm, the netwide assembler
+
+# SYNOPSIS
+
+    use Alien::nasm;
+    # nasm should now be in your PATH if it wasn't already
+
+# DESCRIPTION
+
+# AUTHOR
+
+Graham Ollis <plicease@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Alien/nasm.pm
+++ lib/Alien/nasm.pm
@@ -2,8 +2,31 @@ package Alien::nasm;
 
 use strict;
 use warnings;
+use base qw( Alien::Base );
+use Env qw( @PATH );
+use File::Spec;
 
 # ABSTRACT: Find or build nasm, the netwide assembler
 # VERSION
+
+=head1 SYNOPSIS
+
+ use Alien::nasm;
+ # nasm should now be in your PATH if it wasn't already
+
+=head1 DESCRIPTION
+
+=cut
+
+my $in_path;
+
+sub import
+{
+  return if Alien::nasm->install_type('system');
+  return if $in_path;
+  unshift @PATH, File::Spec->catdir(Alien::nasm->dist_dir, 'bin');
+  # only do it once.
+  $in_path = 1;
+}
 
 1;
